d795cf7a8cdc6d763e4d23b04a8804d3e862f1e0
anze20 <anze20@hotmail.com>
2022-05-09 18:53:21 +0200

    Admin Profile & Image Update

--- app/Http/Controllers/Backend/AdminProfileController.php
+++ app/Http/Controllers/Backend/AdminProfileController.php
@@ -13,4 +13,28 @@ class AdminProfileController extends Controller
         $adminData = Admin::find(1);
         return view('admin.admin_profile_view', compact('adminData'));
     }
+
+    public function AdminProfileEdit()
+    {
+        $editData = Admin::find(1);
+        return view('admin.admin_profile_edit', compact('editData'));
+    }
+
+    public function AdminProfileStore(Request $request)
+    {
+        $data = Admin::find(1);
+        $data->name = $request->name;
+        $data->email = $request->email;
+
+        if ($request->file('profile_photo_path')) { # Če je izbran image
+            $file = $request->file('profile_photo_path');
+            @unlink(public_path('upload/admin_images/' . $data->profile_photo_path));   # Odstrani prejšnjo datoteko.
+            $filename = date('YmdHi') . $file->getClientOriginalName();                 # Ime je datastring + original ime s končnico
+            $file->move(public_path('upload/admin_images'), $filename);                 # Image se shrani v določeno mapo z prej določenim imenom
+            $data['profile_photo_path'] = $filename;                                    # V bazi se ime shrani pod Profile photo path
+        }
+
+        $data->save();
+        return redirect()->route('admin.profile');
+    }
 }
