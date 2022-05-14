656fd2f2a2656f8b3349ed9de1edf9ae9ed4ab5c
anze20 <anze20@hotmail.com>
2022-05-14 17:16:12 +0200

    User Profile Design

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class IndexController extends Controller
 {
@@ -16,6 +19,22 @@ class IndexController extends Controller
     {
         return view('frontend.index');
     }
+
+    public function UserLogout()
+    {
+        Auth::logout();
+        return redirect()->route('login');
+    }
+
+    public function UserProfile()
+    {
+        $id = Auth::user()->id;
+        $user = User::find($id);
+
+        return view('frontend.profile.user_profile', compact('user'));
+    }
+
+
 
     /**
      * Show the form for creating a new resource.
@@ -33,9 +52,30 @@ class IndexController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function UserProfileStore(Request $request)
     {
-        //
+        $data = User::find(Auth::user()->id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+
+        if ($request->file('profile_photo_path')) { # Če je izbran image
+            $file = $request->file('profile_photo_path');
+            @unlink(public_path('upload/user_images/' . $data->profile_photo_path));   # Odstrani prejšnjo datoteko.
+            $filename = date('YmdHi') . $file->getClientOriginalName();                 # Ime je datastring + original ime s končnico
+            $file->move(public_path('upload/user_images'), $filename);                 # Image se shrani v določeno mapo z prej določenim imenom
+            $data['profile_photo_path'] = $filename;                                    # V bazi se ime shrani pod Profile photo path
+        }
+
+        $data->save();
+
+        $notification = array(
+            'message' => 'User Profile Updated Succesfully',
+            'alert-type' => 'success',
+        );
+
+
+        return redirect()->route('dashboard')->with($notification);
     }
 
     /**
